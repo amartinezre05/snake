c615efc73f44e4d2cce57227d936937e4a57b0dc
amartinezre05 <almartinezre05@gmail.com>
2020-02-12 13:51:05 -0500

    [Add] the function to move around the grid

--- src/actions/actions.rb
+++ src/actions/actions.rb
@@ -1,6 +1,6 @@
 module Actions
     def self.move_snake(state)
-        next_direction = state.next_direction
+        next_direction = state.curr_direction
         next_position = calc_next_position(state)
         # verificar que la siguiente casilla sea valida
         if position_is_valid?(state, next_position)
@@ -10,39 +10,44 @@ module Actions
         end
     end
 
+    def self.change_direction(state, direction)
+        if next_direction_is_valid?(state, direction)
+            state.curr_direction = direction
+        else
+            puts "Invalid direction"
+        end
+        state
+    end
+
     private
 
-    def calc_next_position(state)
+    def self.calc_next_position(state)
         curr_position = state.snake.positions.first
-        case state.next_direction
-        when UP
+        case state.curr_direction
+        when Model::Direction::UP
             # decrementar fila
             return Model::Coord.new(
                 curr_position.row - 1,
-                curr_position.col
-            )
-        when RIGHT
+                curr_position.col)
+        when Model::Direction::RIGHT
             # incrementar col
             return Model::Coord.new(
                 curr_position.row,
-                curr_position.col + 1
-            )
-        when DOWN
+                curr_position.col + 1)
+        when Model::Direction::DOWN
             # incrementar fila
             return Model::Coord.new(
                 curr_position.row + 1,
-                curr_position.col
-            )
-        when LEFT
+                curr_position.col)
+        when Model::Direction::LEFT
             # decrementar col
             return Model::Coord.new(
                 curr_position.row,
-                curr_position.col - 1
-            )
+                curr_position.col - 1)
         end
     end
 
-    def position_is_valid?(state, position)
+    def self.position_is_valid?(state, position)
         # verificar que este en grid
         is_invalid = ((position.row >= state.grid.rows || 
             position.row < 0) ||
@@ -53,13 +58,30 @@ module Actions
         return !(state.snake.positions.include? position)
     end
 
-    def move_snake_to(state, next_position)
+    def self.move_snake_to(state, next_position)
         new_positions = [next_position] + state.snake.positions[0...-1]
         state.snake.positions = new_positions
+        state
     end
 
-    def end_game(state)
+    def self.end_game(state)
         state.game_finished = true
+        state
+    end
+
+    def self.next_direction_is_valid?(state, direction)
+        case state.curr_direction
+        when Model::Direction::UP
+            return true if direction != Model::Direction::DOWN
+        when Model::Direction::DOWN
+            return true if direction != Model::Direction::UP
+        when Model::Direction::RIGHT
+            return true if direction != Model::Direction::LEFT
+        when Model::Direction::LEFT
+            return true if direction != Model::Direction::RIGHT
+        end
+
+        return false
     end
 end
 
